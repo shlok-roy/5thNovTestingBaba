50b6ad9130665c3bdb15b0d0f584d1642be28014
shlok-roy <94392826+shlok-roy@users.noreply.github.com>
2025-05-16 21:52:34 +0530

    Add files via upload

---
+++ 5thNovTestingBaba_MavenProject/src/main/java/testingbaba/TestingBabaFirst.java
@@ -0,0 +1,17 @@
+package testingbaba;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestingBabaFirst {
+	
+	@Test
+	public void launchurl()
+	{
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver = new ChromeDriver();
+	}
+}
